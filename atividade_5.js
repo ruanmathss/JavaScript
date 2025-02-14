0781466070b06a188acf63688039b8557e018ba0
Ruan <rnmathss@gmail.com>
2025-02-14 17:53:04 -0300

    Update and rename atividade_5.js to atividades JS

--- atividade_5.js
+++
@@ -1,9 +0,0 @@
-let seuNome = prompt("Qual o seu nome?")
-
-let peso = Number(prompt("Quanto você pesa?"))
-
-let altura = Number(prompt("Qual a sua altura?"))
-
-confirm(`Nome: ${seuNome}\n Peso: ${peso}\n Altura: ${altura}`)
-
-c ? alert : 
