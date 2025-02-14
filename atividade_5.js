3d3cfab11f75b718c1a8131614b376b9778ba54c
Ruan <rnmathss@gmail.com>
2025-02-14 17:51:19 -0300

    Create atividade_5.js

---
+++ atividade_5.js
@@ -0,0 +1,9 @@
+let seuNome = prompt("Qual o seu nome?")
+
+let peso = Number(prompt("Quanto você pesa?"))
+
+let altura = Number(prompt("Qual a sua altura?"))
+
+confirm(`Nome: ${seuNome}\n Peso: ${peso}\n Altura: ${altura}`)
+
+c ? alert : 
